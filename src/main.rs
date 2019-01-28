42272e48c920118268fa67a534560d514bb6f749
Gorm Casper <git@mail.gormcasper.dk>
2019-01-28 17:45:42 +0100

    meh

--- src/main.rs
+++ src/main.rs
@@ -5,28 +5,55 @@ use std::sync::mpsc;
 use std::thread;
 
 fn main() -> io::Result<()> {
-    let (tx, rx) = mpsc::channel();
     let listener = TcpListener::bind("127.0.0.1:9000")?;
 
-    thread::spawn(move || {
-        for stream in listener.incoming() {
-            let mut stream = stream.unwrap();
-            let mut buffer = [0; 512];
-            stream.read(&mut buffer).unwrap();
-            let event = Event::Server(buffer);
-            tx.send(event).unwrap();
+    for stream in listener.incoming() {
+        let stream = stream?;
+        let _ = handle_incoming(stream);
+    }
+
+    Ok(())
+}
+
+fn handle_incoming(mut stream: TcpStream) -> io::Result<()> {
+    let mut stream_clone = stream.try_clone()?;
+    let mut target = TcpStream::connect("127.0.0.1:8080")?;
+    let mut target_clone = target.try_clone()?;
+    let (tx, rx) = mpsc::channel();
+    let tx_1 = tx.clone();
+    let tx_2 = tx.clone();
+
+    thread::spawn(move || loop {
+        let mut buffer = [0; 512];
+        let n = stream_clone.read(&mut buffer).unwrap();
+        if n == 0 {
+            println!("Request terminated");
+            return;
         }
+        let event = Event::Request(buffer, n);
+        tx_1.send(event).unwrap();
+    });
+
+    thread::spawn(move || loop {
+        let mut buffer = [0; 512];
+        let n = target_clone.read(&mut buffer).unwrap();
+        if n == 0 {
+            println!("Response terminated");
+            return;
+        }
+        let event = Event::Response(buffer, n);
+        tx_2.send(event).unwrap();
     });
 
     for event in rx {
         match event {
-            Event::Server(buffer) => {
-                // Handle server requests
-                io::stdout().write(&buffer);
-                // ... send to client
+            Event::Request(buffer, n) => {
+                let _ = io::stdout().write(&buffer);
+                let _ = target.write(&buffer[..n]);
             }
-            _ => {
-                // Handle client response
+            Event::Response(buffer, n) => {
+                let _ = io::stdout().write(&buffer);
+                let _ = stream.write(&buffer[..n]);
             }
         }
     }
@@ -35,21 +62,10 @@ fn main() -> io::Result<()> {
 }
 
 enum Event {
-    Server([u8; 512]),
-    Client([u8; 512]),
+    Request([u8; 512], usize),
+    Response([u8; 512], usize),
 }
-
-/*
-fn handle(mut stream: TcpStream) {
-    println!("Request: {}", String::from_utf8_lossy(&buffer[..]));
-}
-*/
 
 // https://doc.rust-lang.org/std/net/struct.TcpStream.html
 // https://doc.rust-lang.org/std/net/
 // https://github.com/sorribas/tcp-spy/blob/master/index.js
-//
-//
-// let response = "HTTP/1.1 200 OK\r\n\r\n";
-// stream.write(response.as_bytes()).unwrap();
-// stream.flush().unwrap();
